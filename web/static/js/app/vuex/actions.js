d0b2cf5cfdcc1b384146217ca1edaca484d074da
Kurokawa <black.princess.w@gmail.com>
2016-07-13 00:07:32 +0900

    team search sample

--- web/static/js/app/vuex/actions.js
+++ web/static/js/app/vuex/actions.js
@@ -1,9 +1,13 @@
 import * as axios from "axios"
 
 export const fetchTeams = (store, message) => {
-  axios.get("/api/teams").then((res) => {
+  axios.get("/api/teams", {params: store.state.teams.searchForm}).then((res) => {
     store.commit("FETCH_TEAMS", res.data.data)
   })
+}
+
+export const updateTeamsSearchFormNameCont = (store, message) => {
+  store.commit("UPDATE_TEAMS_SEARCH_FORM_NAME_CONT", message.target.value)
 }
 
 export const moveTeamsIndex = (store, message) => {
